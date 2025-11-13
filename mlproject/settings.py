e3090dd7d8bb5ee330d2c10d93e6d1eb6c17abbc
luisrepo25 <blanco.luis@ficct.uagrm.edu.bo>
2025-11-13 02:39:40 -0400

    final we

--- mlproject/settings.py
+++ mlproject/settings.py
@@ -25,9 +25,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-oa=av^t8t93ev@7c0m5(5(#ihecy9_j#tizibl&wkgorlalfzi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config(
+    'ALLOWED_HOSTS',
+    default='localhost,127.0.0.1',
+    cast=lambda v: [s.strip() for s in v.split(',')]
+)
 
 
 # Application definition
